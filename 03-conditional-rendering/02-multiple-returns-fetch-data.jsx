42dc2eb6c56bab210435af384d52ea6ffd04b73a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-21 21:20:34 +0530

    Update 02-multiple-returns-fetch-data.jsx

--- 03-conditional-rendering/02-multiple-returns-fetch-data.jsx
+++ 03-conditional-rendering/02-multiple-returns-fetch-data.jsx
@@ -1,54 +1,54 @@
 import { useEffect, useState } from 'react';
+
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [user, setUser] = useState(null);
+
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const resp = await fetch(url);
-        // console.log(resp);
-        if (!resp.ok) {
-          setIsError(true);
-          setIsLoading(false);
-          return;
-        }
-
         const user = await resp.json();
+        console.log(user);
         setUser(user);
-      } catch (error) {
-        setIsError(true);
-        // console.log(error);
+      } 
+      catch (error) {
+        setIsError(true)
+        console.log(error);
       }
-      // hide loading
-      setIsLoading(false);
+      setIsLoading(false) //as loading is complete
     };
+
     fetchUser();
   }, []);
-  // order matters
+
   // don't place user JSX before loading or error
 
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
+
   if (isError) {
     return <h2>There was an error...</h2>;
   }
-  const { avatar_url, name, company, bio } = user;
+
   return (
     <div>
       <img
-        style={{ width: '150px', borderRadius: '25px' }}
-        src={avatar_url}
-        alt={name}
+        style={{ width: "150px", borderRadius: "25px" }}
+        src={user.avatar_url}
+        alt={user.name}
       />
-      <h2>{name}</h2>
-      <h4>works at {company}</h4>
-      <p>{bio}</p>
+      <h2>{user.name}</h2>
+      <h4>works at {user.company}</h4>
+      <p>{user.bio}</p>
     </div>
   );
 };
+
+
 export default MultipleReturnsFetchData;
