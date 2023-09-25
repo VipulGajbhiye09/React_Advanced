656c40fa4c53b0439d9c55df26fe1efbb006083f
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-25 13:20:46 +0530

    Create 05-short-circuit-examples.jsx

---
+++ 03-conditional-rendering/05-short-circuit-examples.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const ShortCircuitExamples = () => {
+  
+  const [text, setText] = useState('');    // falsy
+  const [name, setName] = useState('susan');    // truthy
+
+  const [user, setUser] = useState({ name: 'john' }); // truthy
+  const [isEditing, setIsEditing] = useState(false);  // falsy
+
+  return (
+    <div>
+      {/* content inside element 
+      if text is true return text else return default value*/}
+      <h2>{text || 'default value'}</h2>
+      
+      {/* toggle element 
+      if text is true return 2nd operand i.e. return div with h2 tags*/}
+      {text && (
+        <div>
+          <h2> whatever return</h2>
+          <h2>{name}</h2>
+        </div>
+      )}
+      
+      
+    </div>
+  );
+};
+
+export default ShortCircuitExamples;
