933675db12434e1339a2fcccacf5a7c2bbe07760
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-26 22:45:43 +0530

    Update 07-user-challenge.jsx

--- 03-conditional-rendering/07-user-challenge.jsx
+++ 03-conditional-rendering/07-user-challenge.jsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
 
+const data={name:'vipul'}
+
 const UserChallenge = () => {
   const [user, setUser] = useState(null);
+
+  const login =()=> {
+    setUser(data.name);
+  };
+  const logout = () => {
+    setUser(null);
+  };
 
   return (
     <div>
       {user ? (
         <div>
-          <h2> Truthy Return </h2>
+          <h2>hello User : {user}</h2>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </div>
       ) : (
         <div>
-          <h3>Falsy Return</h3>
+          <h3>please login</h3>
+          <button className="btn" onClick={login}>
+            Login
+          </button>
         </div>
       )}
     </div>
