3b4c6b72166bd3fef8c3d1f43515197d97ada791
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-27 19:32:54 +0530

    Update 05-cleanup-function.jsx

--- 02-useEffect/05-cleanup-function.jsx
+++ 02-useEffect/05-cleanup-function.jsx
@@ -3,6 +3,8 @@ import { useState,useEffect } from "react";
 const CleanupFunction = () => {
   const [toggle,setToggle] = useState(false);
 
+  //toggling component using short circuit operator
+  //Leads to mounting & un-mounting of RandomComponent
   return (
     <div>
       <button className="btn" onClick={()=>{setToggle(!toggle)}}>Toggle Component </button>
@@ -11,8 +13,16 @@ const CleanupFunction = () => {
   );
 };
 
+//This useEffect should run only on initial render due to empty dependency array but
+//due to conditional rendering of component using && on every mount & unmount function in useEffect will be executed leading to data leak & background executiom
 const RandomComponent=()=>{
+
+  useEffect(()=>{
+    console.log("hmm interesting behavior");
+  },[]);
+
   return <h1>Random Component Content</h1>
 }
+
 
 export default CleanupFunction;
