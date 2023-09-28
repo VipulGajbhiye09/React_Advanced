597c8889ceb82b4f9189b40dbfbb702dfc027595
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-28 18:29:43 +0530

    Update List.jsx

--- 05-leverage-javascript/List.jsx
+++ 05-leverage-javascript/List.jsx
@@ -1,13 +1,13 @@
-import avatar from "../../../assets/default-avatar.svg";
+import avatar from "/assets/default-avatar.svg";
 import Person from "./Person";
-import { people } from "../../../data.js";
+import { people } from "data.js";
 
 const List = () => {
   return (
     <div>
-      {people.map((people) => {
-        const { id, name, nickName, images } = people;
-        const url = images?.[0]?.small?.url || avatar;    
+      {people.map((people) => {      //Array method map 
+        const { id, name, nickName, images } = people; //js optional chaining
+        const url = images?.[0]?.small?.url || avatar;  //short circuit
         return (
           <Person key={id} name={name} nickName={nickName || '----'}  img_url={url} />
         );
