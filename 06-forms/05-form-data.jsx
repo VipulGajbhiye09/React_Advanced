5600cea390808e205ab22f02c352b357dd084986
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-09-30 23:07:28 +0530

    Create 05-form-data.jsx

---
+++ 06-forms/05-form-data.jsx
@@ -0,0 +1,61 @@
+//The FormData interface provides a way to construct a set of key/value pairs representing form fields and their values, which can be sent using the fetch() or XMLHttpRequest.send() method. 
+
+import { useState } from 'react';
+
+const UncontrolledInputs = () => {
+  const [value, setValue] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    // const name = formData.get('name');
+    // console.log(name);
+    // console.log([...formData.entries()]);
+    const newUser = Object.fromEntries(formData);
+    // do something (post request, add to list, etc)
+    console.log(newUser);
+    // Bug - re-render won't clear out the values in input field
+    setValue(value + 1);
+    //to reset values
+    e.currentTarget.reset();
+  };
+  return (
+    <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <h4>Form Data API</h4>
+        {/* name */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+          <input type='text' className='form-input' id='name' name='name' />
+        </div>
+        {/* email */}
+        <div className='form-row'>
+          <label htmlFor='email' className='form-label'>
+            Email
+          </label>
+          <input type='email' className='form-input' id='email' name='email' />
+        </div>
+        {/* password */}
+        <div className='form-row'>
+          <label htmlFor='password' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            className='form-input'
+            id='password'
+            name='password'
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </div>
+  );
+};
+export default UncontrolledInputs;
